9bab39a9ac54689e5195062d26df06f741ba35fd
sheguangrun <sheguangrun@xhsd.com>
2020-07-05 14:01:54 +0800

    增加文档输出索引序号 by guangrun at 20200705

--- src/main/java/cn/xunyard/idea/coding/doc/logic/render/DocumentRender.java
+++ src/main/java/cn/xunyard/idea/coding/doc/logic/render/DocumentRender.java
@@ -32,8 +32,10 @@ public class DocumentRender {
         try (FileOutputStream fos = new FileOutputStream(filepath, false)) {
             try (OutputStreamWriter osw = new OutputStreamWriter(fos, StandardCharsets.UTF_8)) {
                 try (BufferedWriter bufferedWriter = new BufferedWriter(osw)) {
+                    int serviceIndex = 1;//服务索引
                     for (ServiceDescriber serviceDescriber : serviceDescriberList) {
-                        renderService(bufferedWriter, serviceDescriber);
+                        renderService(serviceIndex, bufferedWriter, serviceDescriber);
+                        serviceIndex++;
                     }
 
                     bufferedWriter.flush();
@@ -49,7 +51,70 @@ public class DocumentRender {
         }
     }
 
+    /**
+     *  生成接口文档
+     * @param serviceIndex
+     * @param fileWriter
+     * @param serviceDescriber
+     * @throws IOException
+     */
+    private void renderService(int serviceIndex, BufferedWriter fileWriter, ServiceDescriber serviceDescriber)
+            throws IOException {
+
+        fileWriter.write(String.format("## " + serviceIndex + ". %s\n\n", serviceDescriber.getDescription()));
+        if (!AssertUtils.isEmpty(serviceDescriber.getNote())) {
+            fileWriter.write(String.format("接口说明\n%s\n", serviceDescriber.getNote()));
+        }
+
+        fileWriter.write(String.format("*接口路径*\n\n**%s**\n", serviceDescriber.getFullName()));
+        fileWriter.write("\n\n");
+        int serviceMethodIndex = 1;//服务方法索引
+        for (MethodDescriber methodDescriber : serviceDescriber.getMethods()) {
+            renderMethod(serviceIndex, serviceMethodIndex, fileWriter, methodDescriber);
+            serviceMethodIndex++;
+        }
+    }
+
+    /**
+     * 生成接口中方法文档
+     * @param serviceIndex
+     * @param serviceMethodIndex
+     * @param fileWriter
+     * @param methodDescriber
+     * @throws IOException
+     */
+    private void renderMethod(int serviceIndex, int serviceMethodIndex, BufferedWriter fileWriter,
+                              MethodDescriber methodDescriber)
+            throws IOException {
+
+        fileWriter.write(String.format("### " + serviceIndex + "." + serviceMethodIndex + ". %s\n",
+                methodDescriber.getDescription()));
+        fileWriter.write("\n\n");
+        if (!AssertUtils.isEmpty(methodDescriber.getNote())) {
+            fileWriter.write(String.format("\n>%s\n\n", methodDescriber.getNote()));
+            fileWriter.write("\n\n");
+        }
+
+        fileWriter.write(String.format("#### " + serviceIndex + "." + serviceMethodIndex
+                + ".1 方法签名\n\n```java\n%s\n```\n", methodDescriber.getSign()));
+        fileWriter.write("\n\n");
+        if (methodDescriber.hasParameter()) {
+            fileWriter.write("#### " + serviceIndex + "." + serviceMethodIndex
+                    + ".2 请求参数\n\n");
+            parameterRender.renderParameter(fileWriter, methodDescriber.getParameterList().iterator().next());
+            fileWriter.write("\n\n");
+        }
+
+        if (methodDescriber.hasResponse()) {
+            fileWriter.write("#### " + serviceIndex + "." + serviceMethodIndex
+                    + ".3 返回参数\n\n");
+            responseRender.renderParameter(fileWriter, methodDescriber.getResponse());
+            fileWriter.write("\n\n");
+        }
+    }
+
     private void renderService(BufferedWriter fileWriter, ServiceDescriber serviceDescriber) throws IOException {
+
         fileWriter.write(String.format("## %s\n\n", serviceDescriber.getDescription()));
 
         if (!AssertUtils.isEmpty(serviceDescriber.getNote())) {
