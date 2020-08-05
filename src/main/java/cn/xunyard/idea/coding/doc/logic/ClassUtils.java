522a18bdd15491fa2f963d3e049d7c05e3f6157d
xunyard <wuqi@terminus.io>
2020-08-05 15:26:41 +0800

    support wildcard kind java class resolve

--- src/main/java/cn/xunyard/idea/coding/doc/logic/ClassUtils.java
+++ src/main/java/cn/xunyard/idea/coding/doc/logic/ClassUtils.java
@@ -1,15 +1,26 @@
 package cn.xunyard.idea.coding.doc.logic;
 
+import cn.xunyard.idea.coding.log.Logger;
+import cn.xunyard.idea.coding.log.LoggerFactory;
 import cn.xunyard.idea.coding.util.AssertUtils;
+import com.google.common.base.Throwables;
 import com.thoughtworks.qdox.model.JavaClass;
 import com.thoughtworks.qdox.model.JavaPackage;
 import com.thoughtworks.qdox.model.JavaType;
+import com.thoughtworks.qdox.model.JavaWildcardType;
+import com.thoughtworks.qdox.model.impl.DefaultJavaWildcardType;
+import org.apache.commons.collections.CollectionUtils;
+import org.jetbrains.annotations.Nullable;
+
+import java.lang.reflect.Field;
+import java.util.List;
 
 /**
  * @author <a herf="mailto:wuqi@terminus.io">xunyard</a>
  * @date 2019-12-26
  */
 public class ClassUtils {
+    private static final Logger log = LoggerFactory.getLogger(ProcessContext.IDENTITY);
     public static final String JAVA_SRC_ROOT = "/src/main/java/";
 
     public static boolean isSrcClass(String fullPath) {
@@ -37,6 +48,14 @@ public class ClassUtils {
             return "";
         }
 
+        if (javaClass instanceof JavaWildcardType) {
+            javaClass = (JavaClass) unpackForWildcardType(javaClass);
+        }
+
+        if (javaClass == null) {
+            return "";
+        }
+
         JavaPackage pkg = javaClass.getPackage();
 
         if (pkg == null) {
@@ -60,10 +79,43 @@ public class ClassUtils {
 
     public static String getSimpleName(JavaType javaType) {
         if (javaType instanceof JavaClass) {
-            return ((JavaClass)javaType).getSimpleName();
+            javaType = unpackForWildcardType(javaType);
+            if (javaType == null) {
+                return "";
+            }
+
+            return ((JavaClass) javaType).getSimpleName();
         }
 
         return javaType.getValue();
+    }
+
+    private static @Nullable
+    JavaType unpackForWildcardType(JavaType javaType) {
+        if (!(javaType instanceof JavaWildcardType)) {
+            return javaType;
+        }
+
+        try {
+            Field boundsField = DefaultJavaWildcardType.class.getDeclaredField("bounds");
+            boundsField.setAccessible(true);
+            @SuppressWarnings("unchecked")
+            List<JavaType> bounds = (List<JavaType>) boundsField.get(javaType);
+
+            if (CollectionUtils.isEmpty(bounds)) {
+                return null;
+            } else {
+                if (bounds.size() > 1) {
+                    log.warn("出现无法正确解析的类定义: {}", javaType.getGenericFullyQualifiedName());
+                }
+
+                // 先只取第一个
+                return bounds.iterator().next();
+            }
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            log.error("解析类对象出错: {}", Throwables.getStackTraceAsString(e));
+            return null;
+        }
     }
 
     public static boolean isBasicType(JavaType type) {
